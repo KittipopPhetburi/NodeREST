3a4572c510c941933bc734cb7d30083eae13da1d
KittipopPhetburi <s6506021620016@email.kmutnb.ac.th>
2024-02-06 11:35:47 +0700

    2

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const Book = sequelize.define('book',{
     });
 
     app.get('/books/:id', (req,res) => {
-        Book.findByPk(res.params.id).then(book => {
+        Book.findByPk(req.params.id).then(book => {
             if(!book){
                 res.status(404).send('Book not found');
             } else {
@@ -57,7 +57,7 @@ const Book = sequelize.define('book',{
     });
 
     app.put('/books/:id', (req,res) => {
-        Book.findByPk(res.params.id).then(book => {
+        Book.findByPk(req.params.id).then(book => {
             if(!book){
                 res.status(404).send('Book not found');
             } else {
@@ -73,7 +73,7 @@ const Book = sequelize.define('book',{
     });
 
     app.delete('/books/:id', (req,res) => {
-        Book.findByPk(res.params.id).then(book => {
+        Book.findByPk(req.params.id).then(book => {
             if(!book){
                 res.status(404).send('Book not found');
             } else {
